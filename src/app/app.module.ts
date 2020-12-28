5b6e314a5b0c113a6923bd277a01be8650030655
Hassan Yousef <hassanyousef.de@gmail.com>
2020-12-29 00:53:50 +0100

    Assignment 2: Angular Services, Routing and Single Page Applications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { MatListModule } from '@angular/material/list';
+
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -21,6 +23,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { LeaderService } from './services/leader.service';
 import { PromotionService } from './services/promotion.service';
 
 
@@ -49,6 +52,7 @@ import { AppRoutingModule} from './app-routing/app-routing.module';
   ],
   providers: [
     DishService,
+    LeaderService,
     PromotionService,
   ],
   bootstrap: [AppComponent]
